774b8ea1b69fcef37d9a75fbc806c79960fdb834
SabirAhmed85 <sabir.ahmed@hotmail.co.uk>
2018-01-21 12:50:12 +0000

    legalservebedford files added

---
+++ Website/index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html ng-app="NightLife">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width">
+    <meta http-equiv="Content-Security-Policy" content="default-src gap://ready file://* *; script-src 'self' 'unsafe-inline' 'unsafe-eval' *; style-src 'self' 'unsafe-inline' *">
+    <title>Process Server Bedford &amp; London | Legal Serve</title>
+    <meta name="description" content="Bedfordshire, Buckinghamshire &amp; London-based process server, legal courier &amp; legal document server."/>
+    <meta name="keywords" content="bedford, luton, london, buckinghamshire, process server, legal courier, data protection compliant, process agent, court order server, legal document server, process serving, processing agent, UK, MK40"/>
+
+    <link href="/css/outer-style.css" rel="stylesheet">
+    
+    <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+        
+  </head>
+
+  <body class="index">
+    <div>
+      <div id="header-bar" align-title="left" class="bar-positive initial-header-bar">
+        <span class="top-logo-container">
+            <img src="img/logo.png" alt="Legal Serve - Process Server in Bedfordshire &amp; Buckinghamshire"/>
+            <h2>07853 166 674</h2>
+        </span>
+        <div class="initial-header-buttons isVisible">
+            <button class="whats-open active">
+                <a href="#">Home</a>
+            </button>
+            <button class="whats-around">
+                <a href="process-server-bedford.php">Services</a>
+            </button>
+            <button class="whos-online">
+                <a href="contact.php">Contact Us</a>
+            </button>
+        </div>
+    </div>
+    </div>
+    <div class="header-img">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/vHvd6HaPq_s" frameborder="0" allowfullscreen></iframe>
+        <div class="iframe-banner">
+            <a href="#data-compliance">Read about Legal Serve's commitment to Data-Protection</a>
+        </div>
+        <!--
+        <div class="header-inner-wrap">
+            <p>A process serving company in Bedfordshire &amp; Buckinghamshire, providing local process serving of Court Orders &amp; Legal Documents.</p>
+            <a class="enter-preview-password" href="contact.php"><button class="preview-button">Get in touch with Legal Serve UK</button></a>
+        </div>
+        -->
+        
+        <img src="img/legal-serve-process-serving.jpg" alt="Process Serving &amp; Legal Support" class="background-img" />
+    </div>
+
+    <style>
+    
+    </style>
+    <div class="main-content">
+    	<div class="main-content-topbar">
+            <h2>Welcome to Legal Serve, local Process Servers and Legal Couriers in Bedfordshire &amp; Buckinghamshire.</h2>
+            <p>Find out more about the services we offer.</p>
+        </div>
+    </div>
+
+    <div class="central-container">
+
+        <div class="main-container-2 main-container">
+            <img src="img/court-order-serving.jpg" alt="Legal Support Services, including Court Order serving"/>
+            <div class="text">
+                <h2>Our Services</h2>
+                <p>We offer effective process server and legal support services to our clients, which include solicitors, lawyers, government departments &amp; many others.</p>
+                <a href="process-server-bedford.php">Get full details of the kinds of clients we service...</a>
+            </div>
+        </div>
+
+        <div class="main-container-3 main-container">
+            <div class="text">
+                <h2>Local Process Server Bedford</h2>
+                <p>Our process servers are available to serve all types of Court Orders and legal documents. Click the link below to get a more extensive list of our Process Serving services in Bedford, Buckinghamshire &amp; London. </p>
+                <a href="process-server-bedford.php">Read more about the types of documents we process...</a>
+            </div>
+            <img src="img/process-serving-in-bedford.jpg" alt="Get Process Serving in Bedfordshire and Buckinghamshire"/>
+        </div>
+        <div class="main-container-2 main-container">
+            <img src="img/legal-courier-bedford.jpg" alt="Legal Courier Services in Bedford"/>
+            <div class="text">
+                <h2>Legal Courier</h2>
+                <p>We also offer a range of Legal Courier services to our clients, with a keen focus on Data Protection legislation and best practise at all times.</p>
+                <p>With our courier services, you can rest assured that if you require the delivery of your Legal Documents in Bedford, Luton, Buckinghamshire or London, you'll be in the hands of a reliable and safe legal courier at Legal Serve Bedford.</p>
+                <a href="legal-courier-bedford.php">Get more details of the Legal Courier services we offer...</a>
+            </div>
+        </div>
+
+        <div class="main-container-2 main-container">
+            <div class="text full">
+                <h2>Same Day Pick-up, Delivery &amp; Completion</h2>
+                <p>We pride ourselves on providing a prompt and efficient service at all times, and in line with this our process servers &amp; legal couriers always offer same-day delivery &amp; completion of service to our clients.</p>
+                <a href="process-server-bedford.php">Read more about our high quality services and efficient process servers...</a>
+            </div>
+        </div>
+
+        <div class="main-container-3 main-container">
+            <div class="text full">
+                <a name="data-compliance"></a>
+                <h2>Data Protection Compliance</h2>
+                <p>As a company with reputable clients in Bedford, Buckinghamshire and London - clients who range from government bodies to Law Firms - our process servers &amp; couriers pride themselves on their professional integrity, particularly when it comes to Data Compliance.</p>
+                <a href="legal-courier-bedford.php#data-compliance">Find out about our commitment to Data Protection...</a>
+            </div>
+        </div>
+
+        <div class="main-container-3 main-container">
+            <div class="text">
+                <h2>Court Orders served locally in Bedford &amp; Buckingham</h2>
+                <p>We provide a service covering every town and village in Bedfordshire &amp; Buckinghamshire.</p>
+                <a href="process-server-bedford.php">Find out about our Local Knowledge &amp; Awareness...</a>
+            </div>
+            <img src="img/local-knowledge-bedford.jpg" alt="Local Process Serving in Bedford and Buckinghamshire done with local knowledge" />
+        </div>
+
+
+        <div class="main-container-3 main-container main-container-narrow">
+            <span>
+                <i class="ion-ios-people"></i>
+            </span>
+            <div class="text full">
+                <h2>Our Promise to Our Clients</h2>
+                <p>To serve your documents promptly and in accordance with all CPR rules.</p>
+                <p>To serve the papers in according with your special instructions.</p>
+                <p>To ensure all Data Protection and Human Rights legislation is adhered to.</p>
+                <p>To keep you updated and advise you when service has been completed.</p>
+                <p>To monitor all cases we are dealing with and ensure they are returned to you on time.</p>
+                <p>To ensure that all statements or affadavits are completed correctly.</p>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="footer">
+        <span class="red-span">07853 166 674</span>
+        <img src="img/logo.png" alt="Legal Serve Bedford logo"/>
+
+        <ul>
+            <li><a href="#">Home</a></li>            
+            <li><a href="process-server-bedford.php">Process Serving Bedford</a></li>
+            <li><a href="legal-courier-bedford.php">Legal Courier Bedford</a></li>
+            <li><a href="contact.php">Contact Us</a></li>
+        </ul>
+    </div>
+
+  </body>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-92441562-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+</html>
---
+++ Website/legal-courier-bedford.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html ng-app="NightLife">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width">
+    <meta http-equiv="Content-Security-Policy" content="default-src gap://ready file://* *; script-src 'self' 'unsafe-inline' 'unsafe-eval' *; style-src 'self' 'unsafe-inline' *">
+    <title>Legal Courier in Bedford, Bucks &amp; Lodon | Process Servers</title>
+    <meta name="description" content="Providing Legal Courier services in Bedford &amp; Milton Keynes, in compliance with data-protection policy."/>
+    <meta name="keywords" content="legal courier, process agent, legal document server, court order serving, data protection compliance, timely delivery, cost-effective courier, legal courier bedford, legal courier milton keynes"/>
+
+    <link href="/css/outer-style.css" rel="stylesheet">
+    
+    <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+  </head>
+
+  <body>
+    <div>
+      <div id="header-bar" no-tap-scroll="true" align-title="left" class="bar-positive initial-header-bar">
+        <span class="top-logo-container">
+            <img src="img/logo.png" alt="Legal Serve, providing process serving in Bedford &amp; Buckingham"/>
+            <h2>07853 166 674</h2>
+        </span>
+        <div class="initial-header-buttons isVisible">
+            <button class="whats-open active">
+                <a href="index.php">Home</a>
+            </button>
+            <button class="whats-around">
+                <a href="process-server-bedford.php">Services</a>
+            </button>
+            <button class="whos-online">
+                <a href="contact.php">Contact Us</a>
+            </button>
+        </div>
+      </div>
+    </div>
+    <div class="header-img">
+        <img src="img/legal-serve-process-serving.jpg" alt="Process Serving &amp; Legal Support" class="background-img" />
+    </div>
+
+    <div class="main-content">
+    	<div class="main-content-topbar">
+            <h2>Legal Courier Bedford &amp; Buckinghamshire.</h2>
+            <p>Our legal couriers are very familiar with the Bedfordshire and Buckinghamshire counties, as well as data-compliance regulations and guidelines.</p>
+        </div>
+    </div>
+
+    <div class="central-container">
+
+        <div class="main-container-2  main-container">
+            <img src="img/legal-courier-bedford.jpg" alt="Legal Support Services, including Court Order serving"/>
+            <div class="text">
+                <h2>Our Services</h2>
+                <p>As a provider of efficient and reliable Legal Courier services, we are able to not only offer fast service, but one which is also reliable and based on strong knowledge of the local areas which our couriers service.</p>
+            </div>
+            <div class="text full">
+                <p>Not only are our legal couriers trained to deal professionaly and accurately with all relevant paperwork and administration, but the logistics of our legal courier assigments are always well planned.</p>
+            
+                <p>We offer fast delivery and effective legal courier services to our clients, which include solicitors, lawayers, councils, government departments, corporate companies, charities, insurance companies and private individuals in Bedfordshire &amp; Buckinghamshire.</p>
+            </div>
+        </div>
+
+        <div class="main-container-3  main-container">
+            <a name="data-compliance"></a>
+            <div class="text full">
+                <h2>Data Compliance</h2>
+                <p>Our legal couriers appreciate and understand the important of regulation, particularly Data Compliance Regaultion.</p>
+                <p>As well as this, we also provide courier services to reputable organisation around Bedford &amp; Buckingham, which means we take compliance with data-protection legislation very seriously.</p>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="footer">
+        <span class="red-span">07853 166 674</span>
+        <img src="img/logo.png" alt="Logo of Legal Serve, Bedford." />
+
+        <ul>
+            <li><a href="index.php">Home</a></li>            
+            <li><a href="process-server-bedford.php">Process Serving in Bedford</a></li>
+            <li><a href="#">Legal Courier in Bedford</a></li>
+            <li><a href="contact.php">Contact Us</a></li>
+        </ul>
+    </div>
+
+  </body>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-92441562-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+</html>
